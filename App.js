8c194a847beb3c0e9b6d8af74e39fc129dbc3a6f
dareengamal <76594921+dareengamal@users.noreply.github.com>
2022-06-04 15:08:06 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,25 +1,53 @@
-import React from "react";
-import './App.css';
-import { MessageBox } from 'element-react';
-//import {TextField} from '@material-ui/core'
+n
 
-const App =() =>{
+export const getshippment = async (req, res) => {
+    const shippment = await shippment.findOneAndUpdate({ order_Id: req.params.order_id });
+    res.status(200).send({ body: shippment, message: 'Successfully retrieved shippment' });
+};
 
-    return(
-        <div>
-            <h1> check your order status here...
-            </h1>
-            <h1>order number :</h1> 
-           <input type ="text" />
-           <button > SHOW STATUS</button>
-           <button > CANCEL MY ORDER </button>
-            
-           <Button type="text" onClick={this.onClick.bind(this)}>Click to open the Message Box</Button>
-           onClick() {}
-  
-            
-        </div>
-    );
+export const createshippment = async (req, res) => {
+    try {
+        console.log('[createshippment body]', req.body)
+        const { order_id } = req.body;
+        if (!order_id) return res.status(403).send('order_id is required');
+
+        const shippment = await shippment.findOne({ order_id: order_id });
+        if (!shippment) return res.status(403).send('Document already exists, cannot create');
+
+        const shippmentStatus = 'CREATED';
+
+        const newshippmentDocument = await shippment.create({ order_id: order_id,shippment_status: shippmentStatus });
+        return res.status(200).send({ body: newshippmentDocument, message: 'Successfully created shippment' });
+    }
+    catch (e) {
+        console.log('[createshippment] e', e)
+    }
 }
 
-export default App;
+export const updateshippment = async (req, res) => {
+    try {
+        // const { order_id } = createShipment.order_id;
+        const { order_id } = req.body;
+        console.log('order_id', order_id);
+        if (!order_id) return res.status(403).send('order_id is required');
+
+        const shippment = await shippment.findOne({ order_id: order_id });
+        if (!shippment) return res.status(403).send('could not find order_id');
+
+        // fetch shipment from db for this order_id
+        // determine what the current status is
+        // determine what the next status should be
+        // update the database with new
+        const currentshippmentStatus = shippment.shippment_status;
+        const nextshippmentStatus = {
+            CREATED: 'SHIPPED',
+            SHIPPED: 'DELIVERED'
+        }[currentshippmentStatus];
+
+        const updatedDocument = await shippment.updateOne({ order_id: order_id }, { shippment_status: nextshippmentStatus }, { returnDocument: true });
+        return res.status(200).send({ body: updatedDocument, message: 'Successfully updated order status' });
+
+    } catch (e) {
+        console.log('[updateshippment] e', e)
+    }
+}
--- shippment.js
+++ shippment.js
@@ -1,22 +1,3 @@
-/*import express from 'express';
-
-const router = express.Router();
-
-router.get('/' , ( req , res) => {
-    res.send('THIS WORK');
-});
-
-export default router ;*/
-import express from 'express';
-import { getshippment, updateshippment, createshippment } from '../controllers/App.js';
-
-const router=express.Router();
-
-
- //router.get('/', getProducts);
- router.get('/{order_id}',getshippment);
- router.post('/',createshippment);
- router.patch('/',updateshippment);
-
-
-export default router;
+export const getProducts = async (req,res) => {
+    res.send('qwe')
+}
