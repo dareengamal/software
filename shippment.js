269826e9ef34962425f87ddcb25d6f31ee98449d
dareengamal <76594921+dareengamal@users.noreply.github.com>
2022-06-04 15:04:00 +0200

    Add files via upload

--- shippment.js
+++ shippment.js
@@ -1,22 +1,9 @@
-/*import express from 'express';
-
-const router = express.Router();
-
-router.get('/' , ( req , res) => {
-    res.send('THIS WORK');
+import mongoose from 'mongoose';
+const postSchema=mongoose.Schema({
+    shippment_status:String,
+    order_id:Number,
+    address:String,
+    order_status:String
 });
-
-export default router ;*/
-import express from 'express';
-import { getshippment, updateshippment, createshippment ,deleteshippment} from '../controllers/App.js';
-const router=express.Router();
-
-
- //router.get('/', getProducts);
- router.get('/{order_id}',getshippment);
- router.post('/',createshippment);
- router.patch('/',updateshippment);
- router.delete('/',deleteshippment);
-
-
-export default router;
+const shippment= mongoose.model('shippment',postSchema);
+export default shippment;
